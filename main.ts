67ed83fc0a669a1843bccebb3b56ac8624662a8b
NICOHHHHH <154226368+NICOHHHHH@users.noreply.github.com>
2023-12-18 12:38:47 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,56 @@
-basic.forever(function () {
-	
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    for (let index = 0; index < 10; index++) {
+        music.playMelody("B A G A G F A C5 ", 1000)
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
+    music.playMelody("- - - - - C5 - C5 ", 250)
+    basic.showLeds(`
+        . # # # .
+        . . . # .
+        . . . # .
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # . # .
+        . # # # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . # # .
+        . # . . .
+        . # . . .
+        . # . . .
+        . . # # .
+        `)
+    basic.showLeds(`
+        . # . . #
+        . # . # .
+        . # # . .
+        . # . # .
+        . # . . #
+        `)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . # . . .
+        . # . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # # # .
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
 })
